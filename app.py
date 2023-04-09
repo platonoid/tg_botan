2ef5681846e2f288fa125e3bddd44c5ef29de95e
platonoid <118458829+platonoid@users.noreply.github.com>
2023-04-09 22:49:50 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import os
 from dotenv import load_dotenv
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
+from telegram.ext import CommandHandler, MessageHandler, filters, Application
 import logging
 import telegram
 
@@ -11,12 +11,11 @@ app = Flask(__name__)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-updater = Updater(token = os.getenv('TOKEN'), use_context=True)
-dispatcher = updater.dispatcher
+addon = Application.builder().token('6084013080:AAGX8y5i-XAv514ZvUU5PzC45A8iZorLVW0').build()
 
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Привет! Я бот. Как дела?")
+async def start(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a Chamber, please talk to me!")
 
 
 def reply(update, context):
@@ -26,22 +25,19 @@ def reply(update, context):
     except Exception as e:
         context.bot.send_message(chat_id=update.message.chat_id, text='Ошибка: ' + str(e))
 
-start_handler = CommandHandler('start', start)
-reply_handler = MessageHandler(Filters.text & (~Filters.command), reply)
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(reply_handler)
+
+addon.add_handler(CommandHandler('start', start))
+reply_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), reply)
 
 
 @app.route('/', methods=['POST'])
 def webhook():
-    update = telegram.Update.de_json(request.get_json(force=True), updater.bot)
-    dispatcher.process_update(update)
+    update = telegram.Update.de_json(request.get_json(force=True), addon.bot)
+    addon.process_update(update)
     return 'ok'
 
 
 if __name__ == '__main__':
-    updater.start_webhook(listen="0.0.0.0",
-                          port=8443,
-                          url_path='6084013080:AAGX8y5i-XAv514ZvUU5PzC45A8iZorLVW0')
-    updater.bot.setWebhook(url='https://your-bot-url.com/' + '6084013080:AAGX8y5i-XAv514ZvUU5PzC45A8iZorLVW0')
-    app.run()
+    addon = Application.builder().token("6084013080:AAGX8y5i-XAv514ZvUU5PzC45A8iZorLVW0").build()
+    addon.add_handler(CommandHandler('start', start))
+    addon.run_polling()
